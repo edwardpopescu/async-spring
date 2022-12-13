9f97fab7fd70ad6f0a671eccfe12456cf17c5ab2
Edward Popescu <edward.popescu@ing.com>
2022-12-13 18:33:31 +0200

    remove mvc and webflux

--- src/main/java/com/inghubs/asyncspring/AsyncSpringApplication.java
+++ src/main/java/com/inghubs/asyncspring/AsyncSpringApplication.java
@@ -1,10 +1,13 @@
 package com.inghubs.asyncspring;
 
+import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
+
+import static lombok.AccessLevel.PRIVATE;
 
 @SpringBootApplication
+@NoArgsConstructor(access = PRIVATE)
 public class AsyncSpringApplication {
 
 	public static void main(String[] args) {
