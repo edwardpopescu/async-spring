d47a4c8628a4f16fc6c168a173d8b941408f8d77
Edward Popescu <edward.popescu@ing.com>
2022-12-13 18:37:34 +0200

    remove useless constructor and upgrade spring

--- src/main/java/com/inghubs/asyncspring/AsyncSpringApplication.java
+++ src/main/java/com/inghubs/asyncspring/AsyncSpringApplication.java
@@ -1,13 +1,10 @@
 package com.inghubs.asyncspring;
 
-import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static lombok.AccessLevel.PRIVATE;
 
 @SpringBootApplication
-@NoArgsConstructor(access = PRIVATE)
 public class AsyncSpringApplication {
 
 	public static void main(String[] args) {
