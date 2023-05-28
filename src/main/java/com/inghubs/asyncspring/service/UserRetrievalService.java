a1b22f41cd0ac8bf28b5e82f85f7436890d05b27
Edward Popescu <edward.popescu@ing.com>
2023-05-28 20:35:56 +0300

    update to spring boot 3

--- src/main/java/com/inghubs/asyncspring/service/UserRetrievalService.java
+++ src/main/java/com/inghubs/asyncspring/service/UserRetrievalService.java
@@ -1,14 +1,12 @@
 package com.inghubs.asyncspring.service;
 
 import com.inghubs.asyncspring.model.User;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 
 @Service
-@Slf4j
 public class UserRetrievalService {
 
     private final WebClient client = WebClient.create("http://localhost:8080/hello/wiremock");
